f990d8a6f9bcb17e48f21b0d64ebca27ac6ef964
Martin Mueller-Graf <mmg@Martins-MacBook-Pro.local>
2020-01-29 23:02:15 +0100

    initial commit

---
+++ tests/product-service.test.js
@@ -0,0 +1,10 @@
+// tests/product-service.test.js
+var productService = require('../src/services/product-service')
+
+describe('Product service', function () {
+
+  it('Should call http endpoint', async function () {
+    let weight = await productService.getProductWeight('13')
+    expect(weight).toBe(15.5)
+  })
+})
